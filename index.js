bb2c7ab029fe34f5cc971cb4da58f7b244b58c82
wazpx <stamatiou917@gmail.com>
2025-04-26 10:01:17 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,25 @@
 import express from "express";
 import { createBareServer } from "@tomphttp/bare-server-node";
 import { uvPath } from "@titaniumnetwork-dev/ultraviolet";
-import { readFile } from "fs/promises";
 import { join } from "path";
+import { fileURLToPath } from "url";
 import http from "http";
+
+const __dirname = join(fileURLToPath(import.meta.url), "..");
 
 const app = express();
 const bare = createBareServer("/bare/");
 const server = http.createServer();
 
-app.use(express.static("static"));
+// Serve UV client files (if you're using the UV front-end)
+app.use("/uv/", express.static(uvPath));
 
-app.get("/", async (req, res) => {
-  const html = await readFile(join("static", "index.html"), "utf-8");
-  res.send(html);
+// Serve static files from "public" folder
+app.use(express.static(join(__dirname, "public")));
+
+// Optional fallback to index.html (like if user refreshes on client-side route)
+app.get("*", (req, res) => {
+  res.sendFile(join(__dirname, "public", "index.html"));
 });
 
 server.on("request", (req, res) => {
